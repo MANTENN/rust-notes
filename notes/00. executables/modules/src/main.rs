9e23577b9eb2347689f4ca26631e52e4d3c30fd1
Nazar Maksymchuk <nzrbeats@gmail.com>
2019-05-18 10:20:38 -0700

    Packages: progress

---
+++ notes/00. executables/modules/src/main.rs
@@ -0,0 +1,24 @@
+// mod sound {
+//     fn guitar() {
+        
+//     }
+// }
+
+// Nested Modules
+mod sound {
+    mod instrument {
+        mod woodwind {
+            fn clarinet() {
+
+            }
+        }
+    }
+    mod voice {
+
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
